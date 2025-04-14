ecfd437243879cc3842cd2b9a928463bfb633823
Gabriel <caceresgabriel305@gmail.com>
2025-04-13 21:00:04 -0500

    Eliminación del componente App y actualización del componente Tarea. Se mejoró el diseño de la interfaz de usuario con nuevas clases de TailwindCSS y se añadió un formulario para agregar tareas.

--- src/components/Tarea.tsx
+++ src/components/Tarea.tsx
@@ -1,10 +1,18 @@
 import { useState } from 'react'
 import '@styles/App.css'
+import '@styles/global.css'; 
 
 function Tarea() {
+    const [count, setCount] = useState(0)
     return (
-        <div>
-            <h1>Tarea</h1>
+        <div className='flex flex-col items-center justify-center h-screen'>
+            <h1 className='text-6xl font-bold text-center mb-4'>Todo List</h1>
+            <p className='text-center mb-4'>Agrege sus tareas</p>
+            <form className='flex flex-col items-center justify-center' method='post'>
+                <input className='border-2 border-gray-300 rounded-md p-2 mb-4' type="text" placeholder="Add a new task" />
+                <button className='bg-blue-500 text-white p-2 rounded-md w-full mb-5'>Add</button>
+            </form>
+            <button className='bg-red-500 text-white p-2 rounded-md w-full'>Delete</button>
         </div>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@styles/index.css'
-import App from '@components/App'
 import Tarea from '@components/Tarea'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
     <Tarea />
   </StrictMode>,
 )
