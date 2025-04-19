c758aaf7c3cf24a7a19087f1850d2c6d12fec2d0
Gabriel <caceresgabriel305@gmail.com>
2025-04-18 23:41:38 -0500

    Eliminación de la referencia a la prueba de la librería de iconos en main.tsx y actualización del título de la aplicación a "Todo List" en el componente Tarea.

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import '@styles/index.css'
 import Tarea from '@/components/Tarea'
 import UserCard from '@/components/UserCard'
-// Prueba de la libreria de iconos
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
